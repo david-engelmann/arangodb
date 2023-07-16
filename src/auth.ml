11c8d0f54e2fbd1d55893c04bf4aa44ef7457ae9
David Engelmann <david+4113@satisfilabs.com>
2023-07-16 13:25:33 -0400

    functions for the auth headers

--- src/auth.ml
+++ src/auth.ml
@@ -1,18 +1,25 @@
 module Auth = struct
-    type auth =
-      {
-        exp : int;
-        iat : int;
-        scope : string;
-        did : string;
-        jti : string option;
-        token : string;
-        refresh_token : string option;
-      }
+  type auth =
+    {
+      exp : int;
+      iat : int;
+      scope : string;
+      did : string;
+      jti : string option;
+      token : string;
+      refresh_token : string option;
+    }
 
-    type basic_cred =
-      {
-        username : string;
-        password : string;
-      }
+  type basic_cred =
+    {
+      username : string;
+      password : string;
+    }
+
+  let create_basic_auth_header (cred : basic_cred) : (string * string) =
+    ("authorization", ("Basic " ^ cred.username ^ ":" ^ cred.password))
+
+  let create_bearer_auth_header (auth : auth) : (string * string) =
+    ("authorization", ("Bearer " ^ auth.token))
+
 end
