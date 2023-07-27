29b89f9b65b369edf9ce83e77ce29c8c06ec8be3
David Engelmann <david+4113@satisfilabs.com>
2023-07-26 22:22:02 -0400

    run testing script from github action

--- docker/scripts/dune_test.sh
+++ docker/scripts/dune_test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export ARANGO_HOST=$(ping -c 1 coordinator | grep -Po '(?<=\().*(?=\))' | grep -Po '.*(?=\))')
 export ARANGO_PORT=8529
-dune test
+dune test 2>&1 | tee dune_runtest.log
 exec "$@"
