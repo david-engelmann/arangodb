25f7018c73c02feae62ae3e8b0a44bb6aff42041
David Engelmann <david+4113@satisfilabs.com>
2023-07-27 23:29:34 -0400

    use $ for end of line not \s

--- docker/scripts/dune_test.sh
+++ docker/scripts/dune_test.sh
@@ -2,8 +2,8 @@
 #export ARANGO_HOST=$(ping -c 1 coordinator | grep -Po '(?<=\().*(?=\))' | grep -Po '.*(?=\))')
 #export ARANGO_PORT=8529
 docker ps
-export ARANGO_HOST=$(docker ps | grep "coordinator\s" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[1]}' )
-export ARANGO_PORT=$(docker ps | grep "coordinator\s" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[2]}' )
+export ARANGO_HOST=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[1]}' )
+export ARANGO_PORT=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[2]}' )
 echo "HOST NAME PASSED TO TEST"
 echo $ARANGO_HOST
 echo "HOST PORT PASSED TO TEST"
