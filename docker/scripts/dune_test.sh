02ff21d3c220723b6d56e375d9f83258ec0e6cf9
David Engelmann <david+4113@satisfilabs.com>
2023-07-29 12:07:31 -0400

    inspect the networks

--- docker/scripts/dune_test.sh
+++ docker/scripts/dune_test.sh
@@ -3,6 +3,10 @@
 #export ARANGO_PORT=8529
 echo "Inspect Docker Networks"
 docker network ls
+echo "Inspect Bridge"
+docker inspect bridge
+echo "Inspect arangodb-net"
+docker inspect arangodb-net
 #export ARANGO_HOST=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[1]}' )
 #export ARANGO_PORT=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[2]}' )
 export ARANGO_HOST=172.18.0.0
@@ -10,6 +14,26 @@ export ARANGO_PORT=8000
 echo "HOST NAME PASSED TO TEST 1"
 echo $ARANGO_HOST
 echo "HOST PORT PASSED TO TEST 1"
+echo $ARANGO_PORT
+telnet $ARANGO_HOST $ARANGO_PORT
+nmap $ARANGO_HOST
+nslookup $ARANGO_HOST
+echo "------------------------------------------"
+export ARANGO_HOST=0.0.0.0
+export ARANGO_PORT=43849
+echo "HOST NAME PASSED TO TEST 3"
+echo $ARANGO_HOST
+echo "HOST PORT PASSED TO TEST 3"
+echo $ARANGO_PORT
+telnet $ARANGO_HOST $ARANGO_PORT
+nmap $ARANGO_HOST
+nslookup $ARANGO_HOST
+echo "------------------------------------------"
+export ARANGO_HOST=0.0.0.0
+export ARANGO_PORT=49760
+echo "HOST NAME PASSED TO TEST 4"
+echo $ARANGO_HOST
+echo "HOST PORT PASSED TO TEST 4"
 echo $ARANGO_PORT
 telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
@@ -25,25 +49,26 @@ telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
 nslookup $ARANGO_HOST
 echo "------------------------------------------"
-export ARANGO_HOST=coordinator
-export ARANGO_PORT=8000
-echo "HOST NAME PASSED TO TEST 3"
+export ARANGO_HOST=127.0.0.11
+export ARANGO_PORT=49760
+echo "HOST NAME PASSED TO TEST 2"
 echo $ARANGO_HOST
-echo "HOST PORT PASSED TO TEST 3"
+echo "HOST PORT PASSED TO TEST 2"
 echo $ARANGO_PORT
 telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
 nslookup $ARANGO_HOST
 echo "------------------------------------------"
-export ARANGO_HOST=coordinator
-export ARANGO_PORT=8000
-echo "HOST NAME PASSED TO TEST 4"
+export ARANGO_HOST=127.0.0.11
+export ARANGO_PORT=43849
+echo "HOST NAME PASSED TO TEST 2"
 echo $ARANGO_HOST
-echo "HOST PORT PASSED TO TEST 4"
+echo "HOST PORT PASSED TO TEST 2"
 echo $ARANGO_PORT
 telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
 nslookup $ARANGO_HOST
+echo "------------------------------------------"
 netstat -ap
 netstat -g
 netstat -r
