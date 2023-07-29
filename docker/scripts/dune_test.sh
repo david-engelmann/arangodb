53b222dc149b3dffe9c6f5a08610f5fca0f8d282
David Engelmann <david+4113@satisfilabs.com>
2023-07-29 13:23:14 -0400

    try with docker container id

--- docker/scripts/dune_test.sh
+++ docker/scripts/dune_test.sh
@@ -7,8 +7,6 @@ echo "Inspect Bridge"
 docker inspect bridge
 echo "Inspect arangodb-net"
 docker inspect arangodb-net
-#export ARANGO_HOST=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[1]}' )
-#export ARANGO_PORT=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[2]}' )
 export ARANGO_HOST=172.18.0.0
 export ARANGO_PORT=8000
 echo "HOST NAME PASSED TO TEST 1"
@@ -59,11 +57,11 @@ telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
 nslookup $ARANGO_HOST
 echo "------------------------------------------"
-export ARANGO_HOST=127.0.0.11
-export ARANGO_PORT=43849
-echo "HOST NAME PASSED TO TEST 2"
+export ARANGO_HOST=$(docker ps | grep "coordinator$" | awk '{ print $1 }')
+export ARANGO_PORT=$(docker ps | grep "coordinator$" | awk '{split($12,a,"->"); print a[1] }' | awk '{split($1,a,":"); print a[2]}' )
+echo "HOST NAME PASSED TO TEST Final"
 echo $ARANGO_HOST
-echo "HOST PORT PASSED TO TEST 2"
+echo "HOST PORT PASSED TO TEST Final"
 echo $ARANGO_PORT
 telnet $ARANGO_HOST $ARANGO_PORT
 nmap $ARANGO_HOST
