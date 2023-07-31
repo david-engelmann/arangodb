b50fcc216ed5f57d6c84eacb7dd891948ed01d34
David Engelmann <david+4113@satisfilabs.com>
2023-07-31 14:45:06 -0400

    add test for token header creation

--- test/test_session.ml
+++ test/test_session.ml
@@ -19,12 +19,28 @@ let test_token_from_session _ =
   let token = test_session.auth.token in
   OUnit2.assert_equal ~printer:string_of_bool true ((String.length token) > 0)
 
+let test_token_header_from_session_param _ =
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let token_header = Session.token_header_from_session test_session in
+  match token_header with
+  | (param, _) ->
+    OUnit2.assert_bool "Token Header Parameter Name is empty" (param <> "")
+
+let test_token_header_from_session_header _ =
+  let open Core in
+  let test_session = create_test_session () |> Session.refresh_session_auth in
+  let token_header = Session.token_header_from_session test_session in
+  match token_header with
+  | (_, header) ->
+    OUnit2.assert_bool "Token Header is doesn't contain Bearer" (String.is_substring header ~substring:"Bearer")
+
 
 let suite =
   "suite"
   >::: [
          "test_create_session" >:: test_create_session;
          "test_token_from_session" >:: test_token_from_session;
+         "test_token_header_from_session_header" >:: test_token_header_from_session_header;
        ]
 
 let () = run_test_tt_main suite
