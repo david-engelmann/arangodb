32d9184fd63d9aed2f4a95201f3489399006be8c
David Engelmann <david+4113@satisfilabs.com>
2023-07-31 12:30:55 -0400

    run with reinstall

--- docker/scripts/docker_install.sh
+++ docker/scripts/docker_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 apt-get update -qq
 apt-get install apt-transport-https ca-certificates gnupg lsb-release git curl iputils-ping telnet nmap net-tools dnsutils -y
+apt-get upgrade -y
+apt-get install --reinstall iptables
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 exec "$@"
