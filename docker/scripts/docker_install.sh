fc30d197fc6ae1948c977a2d0aaa44e1d2246b78
David Engelmann <david+4113@satisfilabs.com>
2023-07-31 16:03:35 -0400

    try to get iptables still

--- docker/scripts/docker_install.sh
+++ docker/scripts/docker_install.sh
@@ -3,6 +3,7 @@ apt-get update -qq
 apt-get install apt-transport-https ca-certificates gnupg lsb-release git curl iputils-ping telnet nmap net-tools dnsutils -y
 apt-get upgrade -y
 apt-get install --reinstall -y iptables
+dpkg -S /lib/modules/$(uname -r)/kernel/net/ipv4/netfilter/ip_tables.ko
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 exec "$@"
